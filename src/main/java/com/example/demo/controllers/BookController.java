5e25feaeca6e0e288f4531d573c0e22da2808bd7
khanZhomart <jtalgatulu@yandex.kz>
2022-03-28 10:55:56 +0600

    Создал дефолтные эндпоинты в контроллерах

--- src/main/java/com/example/demo/controllers/BookController.java
+++ src/main/java/com/example/demo/controllers/BookController.java
@@ -1,5 +1,50 @@
 package com.example.demo.controllers;
 
+import com.example.demo.entities.Book;
+import com.example.demo.services.BookService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/book")
+@AllArgsConstructor
 public class BookController {
-    
+    private BookService bookService;
+
+    @GetMapping("/")
+    public ResponseEntity<?> findAll() {
+        return ResponseEntity.ok(
+            this.bookService.findAll()
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(
+            this.bookService.findById(id)
+        );
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> save(@RequestBody Book payload) {
+        return ResponseEntity.ok(
+            this.bookService.save(payload)
+        );
+    }
+
+    @PostMapping("/remove")
+    public ResponseEntity<?> removeById(@RequestParam Long id) {
+        return ResponseEntity.ok(
+            this.bookService.removeById(id)
+        );
+    }
 }
--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controllers;
 
+import com.example.demo.entities.User;
 import com.example.demo.services.UserService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -27,6 +31,20 @@ public class UserController {
     public ResponseEntity<?> findById(@PathVariable Long id) {
         return ResponseEntity.ok(
             this.userService.findById(id)
+        );
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> save(@RequestBody User payload) {
+        return ResponseEntity.ok(
+            this.userService.save(payload)
+        );
+    }
+
+    @PostMapping("/remove")
+    public ResponseEntity<?> removeById(@RequestParam Long id) {
+        return ResponseEntity.ok(
+            this.userService.removeById(id)
         );
     }
 }

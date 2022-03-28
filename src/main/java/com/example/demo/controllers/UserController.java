78b958a815028d264e8163ef0fe92fcccaf2da78
khanZhomart <jtalgatulu@yandex.kz>
2022-03-28 11:47:08 +0600

    Рефакторинг кода

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -13,12 +13,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/user")
 @AllArgsConstructor
-@Slf4j
 public class UserController {
     private final UserService userService;
 
